bc50fba404d46e1cb212e69c6a46e659bf996444
asoffer <andy.soffer@gmail.com>
2023-12-10 10:56:36 -0500

    Support for querying instruction metadata.

---
+++ jasmin/core/metadata.h
@@ -0,0 +1,74 @@
+#ifndef JASMIN_CORE_METADATA_H
+#define JASMIN_CORE_METADATA_H
+
+#include <cstddef>
+#include <utility>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "jasmin/core/instruction.h"
+#include "jasmin/core/internal/instruction_traits.h"
+#include "jasmin/core/value.h"
+#include "nth/debug/debug.h"
+
+namespace jasmin {
+
+struct InstructionMetadata {
+  size_t immediate_value_count;
+  size_t parameter_count;
+  Value function;
+};
+
+struct InstructionSetMetadata {
+  explicit InstructionSetMetadata(std::vector<InstructionMetadata> metadata)
+      : metadata_(std::move(metadata)) {
+    for (auto const& m : metadata_) {
+      op_code_.emplace(m.function.as<internal::exec_fn_type>(),
+                       op_code_.size());
+    }
+  }
+
+  size_t size() const { return metadata_.size(); }
+
+  InstructionMetadata metadata(size_t op_code) const {
+    NTH_REQUIRE((v.harden), op_code < metadata_.size());
+    return metadata_[op_code];
+  }
+
+  InstructionMetadata metadata_by_function(Value f) const {
+    auto iter = op_code_.find(f.as<internal::exec_fn_type>());
+    NTH_REQUIRE((v.harden), iter != op_code_.end());
+    return metadata(iter->second);
+  }
+
+ private:
+  std::vector<InstructionMetadata> metadata_;
+  absl::flat_hash_map<internal::exec_fn_type, size_t> op_code_;
+};
+
+namespace internal {
+
+// `Metadata<I>` evaluates to an `InstructionMetadata` object holding metadata
+// about the instruction `I`.
+template <InstructionType I, InstructionSetType Set>
+InstructionMetadata Metadata = InstructionMetadata{
+    .immediate_value_count = ImmediateValueCount<I>(),
+    .parameter_count       = ParameterCount<I>(),
+    .function              = Value(&I::template ExecuteImpl<Set>),
+};
+
+}  // namespace internal
+
+// `Metadata<Set>` evaluates to an `InstructionSetMetadata` object holding
+// metadata about the instruction set `Set`.
+template <InstructionSetType Set>
+InstructionSetMetadata Metadata = Set::instructions.reduce([](auto... is) {
+  return InstructionSetMetadata({internal::Metadata<nth::type_t<is>, Set>...});
+});
+
+template <InstructionType I, InstructionSetType Set>
+Value InstructionFunction = Value(&I::template ExecuteImpl<Set>);
+
+}  // namespace jasmin
+
+#endif  // JASMIN_CORE_METADATA_H
---
+++ jasmin/core/metadata_test.cc
@@ -0,0 +1,62 @@
+#include "metadata.h"
+
+#include "nth/test/test.h"
+
+namespace jasmin {
+
+template <int N>
+struct Inst : jasmin::Instruction<Inst<N>> {
+  static constexpr bool execute(std::span<Value, N>, bool b) { return b; }
+};
+
+struct ImmediateDetermined : jasmin::Instruction<ImmediateDetermined> {
+  static constexpr void execute(std::span<Value>, std::span<Value>, int, bool,
+                                char) {}
+};
+
+using Instructions =
+    jasmin::MakeInstructionSet<Inst<0>, Inst<4>, ImmediateDetermined>;
+
+NTH_TEST("metadata") {
+  auto m = Metadata<Instructions>;
+  NTH_EXPECT(m.size() == Instructions::instructions.size());
+
+  NTH_EXPECT(m.metadata(0).function.raw_value() ==
+             InstructionFunction<Call, Instructions>.raw_value());
+
+  NTH_EXPECT(m.metadata(1).function.raw_value() ==
+             InstructionFunction<Jump, Instructions>.raw_value());
+  NTH_EXPECT(m.metadata(1).immediate_value_count == 1);
+  NTH_EXPECT(m.metadata(1).parameter_count == 0);
+
+  NTH_EXPECT(m.metadata(2).function.raw_value() ==
+             InstructionFunction<JumpIf, Instructions>.raw_value());
+  NTH_EXPECT(m.metadata(2).immediate_value_count == 1);
+  NTH_EXPECT(m.metadata(2).parameter_count == 1);
+
+  NTH_EXPECT(m.metadata(3).function.raw_value() ==
+             InstructionFunction<Return, Instructions>.raw_value());
+  NTH_EXPECT(m.metadata(3).immediate_value_count == 0);
+  NTH_EXPECT(m.metadata(3).parameter_count == 0);
+
+  NTH_EXPECT(m.metadata(4).function.raw_value() ==
+             InstructionFunction<Inst<0>, Instructions>.raw_value());
+  NTH_EXPECT(m.metadata(4).immediate_value_count == 1);
+  NTH_EXPECT(m.metadata(4).parameter_count == 0);
+
+  NTH_EXPECT(m.metadata(5).function.raw_value() ==
+             InstructionFunction<Inst<4>, Instructions>.raw_value());
+  NTH_EXPECT(m.metadata(5).immediate_value_count == 1);
+  NTH_EXPECT(m.metadata(5).parameter_count == 4);
+
+  NTH_EXPECT(
+      m.metadata(6).function.raw_value() ==
+      InstructionFunction<ImmediateDetermined, Instructions>.raw_value());
+  // The immediate value count is 4. Three values are passed to `execute`, and a
+  // fourth implicit value dictates the size of the input and output spans.
+  NTH_EXPECT(m.metadata(6).immediate_value_count == 4);
+  NTH_EXPECT(m.metadata(6).parameter_count ==
+             std::numeric_limits<size_t>::max());
+}
+
+}  // namespace jasmin
